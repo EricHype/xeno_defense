8ff1bbd9b473e25234d59909d07b490d89af41aa
Eric <eheitmuller@gmail.com>
2019-08-03 13:00:05 -0700

    Enemies are now bound to gravity

--- actors/enemy.py
+++ actors/enemy.py
@@ -22,14 +22,31 @@ class Enemy(Entity):
     def update(self, platforms):
         if(not self.isActive):
             return
+     
+        if(not self.onGround):
+            # only accelerate with gravity if in the air
+            self.yvel += 0.3
+            # max falling speed
+            if self.yvel > 100: self.yvel = 100
+        
         # increment in x direction
         self.rect.left += self.xvel
-        # do x-axis collisions
+
+         # do x-axis collisions
         self.collide(self.xvel, 0, platforms)
+        # increment in y direction
+        self.rect.top += self.yvel
+        # assuming we're in the air
+        self.onGround = False;
+        # do y-axis collisions
+        self.collide(0, self.yvel, platforms)
+
 
     def collide(self, xvel, yvel, platforms):
         if(not self.isActive):
             return
+
+        self.onGround = False
 
         for p in platforms:
             if pygame.sprite.collide_rect(self, p):
@@ -41,6 +58,13 @@ class Enemy(Entity):
                     self.rect.left = p.rect.right
                     print("enemy collide left")
                     self.xvel = self.speed;
+                if yvel > 0:
+                    self.rect.bottom = p.rect.top
+                    self.onGround = True
+                    self.yvel = 0
+                if yvel < 0:
+                    self.rect.top = p.rect.bottom
+                    self.yvel =0
     
     def die(self):
         self.isActive = False
